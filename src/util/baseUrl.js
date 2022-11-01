418dcf0ffabd179042901730013703a664ff0e03
Khaled Mahfoz <khaledmahfoz@users.noreply.github.com>
2022-11-01 21:36:28 +0200

    Update baseUrl.js

--- src/util/baseUrl.js
+++ src/util/baseUrl.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'https://order-easy.herokuapp.com/'
+export const baseUrl = 'https://ordereasy.vercel.app/'
 // export const baseUrl = 'http://localhost:8080/'
-export const baseUrlAdmin = 'https://order-easy.herokuapp.com/admin/'
+export const baseUrlAdmin = 'https://ordereasy.vercel.app/admin/'
